ba613ca0c7794e83e1d5c7de1f9f25ec3a02f28b
Thomas Wood <thomas.wood@intel.com>
2011-05-25 11:47:27 +0100

    main: fix logic check for ensuring at least one grilo plugin was loaded

--- shell/mex-main.c
+++ shell/mex-main.c
@@ -2254,8 +2254,8 @@ main (int argc, char **argv)
       if (!grl_plugin_registry_load_by_id (registry, "grl-tracker", NULL))
         {
           /* try and load the upnp and filesystem plugins instead */
-         if (grl_plugin_registry_load_by_id (registry, "grl-upnp", NULL) ||
-          grl_plugin_registry_load_by_id (registry, "grl-filesystem", NULL))
+         if (!grl_plugin_registry_load_by_id (registry, "grl-upnp", NULL) ||
+             !grl_plugin_registry_load_by_id (registry, "grl-filesystem", NULL))
            g_warning ("We had no config and all the fallback grilo-plugins can not be loaded, please check that grilo-plugins has been correctly installed");
         }
     }
