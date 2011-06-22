cf468a64bf903614c0944ae8f6cb8cb600903a0a
Michael Wood <michael.g.wood@linux.intel.com>
2011-06-22 16:37:25 +0100

    webremote: Add correct copyright year and blurb

--- applets/webremote/dbus-interface.h
+++ applets/webremote/dbus-interface.h
@@ -1,6 +1,7 @@
-/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
 /*
- * Copyright 2009, 2010 Intel Corporation.
+ * Mex - a media explorer
+ *
+ * Copyright © 2011 Intel Corporation.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU Lesser General Public License,
@@ -8,14 +9,11 @@
  *
  * This program is distributed in the hope it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
- * more details.
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- * Boston, MA 02111-1307, USA.
- *
+ * along with this program; if not, see <http://www.gnu.org/licenses>
  */
 
 #ifndef __DBUS_INTERFACE_H__
@@ -50,4 +48,4 @@ httpdbus_send_keyvalue (HTTPDBusInterface *dbus_interface, gint keyval);
 
 G_END_DECLS
 
-#endif 
+#endif
