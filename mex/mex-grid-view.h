ec2b2252f665367b2fe31a15e6c3bcc134e9a23a
Lionel Landwerlin <lionel.g.landwerlin@linux.intel.com>
2011-07-18 15:17:38 +0100

    grid-view: use proper headers

--- mex/mex-grid-view.h
+++ mex/mex-grid-view.h
@@ -1,4 +1,20 @@
-/* mex-grid-view.h */
+/*
+ * Mex - a media explorer
+ *
+ * Copyright © 2011 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>
+ */
 
 #ifndef _MEX_GRID_VIEW_H
 #define _MEX_GRID_VIEW_H
