82b8a755d6e2c31b1168b4ab1f29edb1bdb86be8
Lionel Landwerlin <lionel.g.landwerlin@linux.intel.com>
2011-08-26 01:20:14 +0100

    player: use new plugin-manager & player infrastructure

--- player/main.c
+++ player/main.c
@@ -16,6 +16,10 @@
  * along with this program; if not, see <http://www.gnu.org/licenses>
  */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <dbus/dbus-glib-lowlevel.h>
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-bindings.h>
@@ -26,21 +30,44 @@
 #include <mex/mex-media-dbus-bridge.h>
 #include <mex/mex-surface-player.h>
 
+static MexSurfacePlayer *media_player = NULL;
+
+static ClutterMedia *
+_get_media_player (void)
+{
+  return CLUTTER_MEDIA (media_player);
+}
+
 int
 main (int    argc,
       char **argv)
 {
+  MexPluginManager *pmanager;
   MexMediaDBUSBridge *bridge;
-  MexSurfacePlayer *media;
   GMainLoop *loop;
   GError *error = NULL;
+  gchar **plugin_directories;
+
+  plugin_directories = g_new0 (gchar *, 2);
+  plugin_directories[0] = g_strdup (MEX_PLAYER_PLUGIN_PATH);
 
   g_thread_init (NULL);
+
   gst_init (&argc, &argv);
   mex_init (&argc, &argv);
 
-  media = mex_surface_player_new ();
-  bridge = mex_media_dbus_bridge_new (CLUTTER_MEDIA (media));
+  /* Setup player and expose it to mex */
+  media_player = mex_surface_player_new ();
+  mex_player_set_media_player_callback (_get_media_player);
+
+  bridge = mex_media_dbus_bridge_new (CLUTTER_MEDIA (media_player));
+
+  /* Load player's plugins */
+  pmanager = mex_plugin_manager_get_default ();
+  g_object_set (G_OBJECT (pmanager), "search-paths", plugin_directories, NULL);
+  mex_plugin_manager_refresh (pmanager);
+
+  /* Export player's interface on d-bus */
   if (!mex_media_dbus_bridge_register (bridge, &error))
     {
       g_error ("Error registering on D-BUS: %s", error->message);
