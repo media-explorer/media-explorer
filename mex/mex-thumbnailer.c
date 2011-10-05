0c1932f16bc2dbefe644a70fdb33156beb9c9356
Thomas Wood <thomas.wood@intel.com>
2011-10-05 16:57:17 +0100

    Remove the optional use of Tumbler to generate thumbnails
    The use of Tumbler required some additional patches to Tumbler to work
    correctly and the internal thumbnailer now provides the required
    functionality.

--- mex/mex-thumbnailer.c
+++ mex/mex-thumbnailer.c
@@ -23,19 +23,9 @@
 #include <gio/gio.h>
 #include <clutter/clutter.h>
 
-#if defined (HAVE_DBUS_GLIB) && defined (WITH_THUMBNAILER_TUMBLER)
-#include <dbus/dbus-glib.h>
-#endif
-
 #include "mex-thumbnailer.h"
 #include "mex-os.h"
 #include "mex-marshal.h"
-
-#ifdef WITH_THUMBNAILER_TUMBLER
-static DBusGProxy *thumb_proxy = NULL;
-/* Hash of integer handles to callback data */
-static GHashTable *pending = NULL;
-#endif
 
 gchar *
 mex_get_thumbnail_path_for_uri (const gchar *uri)
@@ -47,15 +37,10 @@ mex_get_thumbnail_path_for_uri (const gchar *uri)
 
   md5 = g_compute_checksum_for_string (G_CHECKSUM_MD5, uri, -1);
 
-#ifdef WITH_THUMBNAILER_INTERNAL
   basepath = g_build_filename (g_get_user_cache_dir (), "mex", "thumbnails",
                                NULL);
   file = g_strconcat (md5, ".jpg", NULL);
-#else
-  basepath = g_build_filename (g_get_home_dir (), ".thumbnails", "x-huge",
-                               NULL);
-  file = g_strconcat (md5, ".png", NULL);
-#endif
+
   g_free (md5);
 
   g_mkdir_with_parents (basepath, 0777);
@@ -68,8 +53,6 @@ mex_get_thumbnail_path_for_uri (const gchar *uri)
   return path;
 }
 
-#ifdef WITH_THUMBNAILER_INTERNAL
-/* internal thumbnailer */
 static GThreadPool *thumbnail_thread_pool = NULL;
 
 static char * get_mime_type (const char *uri);
@@ -184,90 +167,6 @@ mex_internal_thumbnail (const gchar          *uri,
       return;
     }
 }
-#endif
-
-#ifdef WITH_THUMBNAILER_TUMBLER
-typedef struct {
-  MexThumbnailCallback callback;
-  gpointer user_data;
-} ClosureData;
-
-static void
-on_ready (DBusGProxy *proxy, guint handle, char **uris)
-{
-  ClosureData *data;
-
-  data = g_hash_table_lookup (pending, GUINT_TO_POINTER (handle));
-  if (data == NULL)
-    return;
-
-  /* Assumption is that its one URI per handle */
-  if (data->callback)
-    data->callback (uris[0], data->user_data);
-  g_free (data);
-}
-
-static void
-on_error (DBusGProxy *proxy, guint handle, char **failed_uris, int error_code, const char *error_message)
-{
-  ClosureData *data;
-
-  g_message ("Cannot generate thumbnail for %s: %s",
-             failed_uris[0], error_message);
-
-  data = g_hash_table_lookup (pending, GUINT_TO_POINTER (handle));
-  g_free (data);
-}
-
-static gboolean
-mex_thumbnailer_init (void)
-{
-  DBusGConnection *connection;
-
-  if (thumb_proxy)
-    return TRUE;
-
-  pending = g_hash_table_new (NULL, NULL);
-
-  connection = dbus_g_bus_get (DBUS_BUS_SESSION, NULL);
-  if (!connection)
-    return FALSE;
-
-  thumb_proxy = dbus_g_proxy_new_for_name (connection,
-                                     "org.freedesktop.thumbnails.Thumbnailer1",
-                                     "/org/freedesktop/thumbnails/Thumbnailer1",
-                                     "org.freedesktop.thumbnails.Thumbnailer1");
-
-  dbus_g_object_register_marshaller (mex_marshal_VOID__UINT_STRING,
-                                     G_TYPE_NONE,
-                                     G_TYPE_UINT,
-                                     G_TYPE_STRV,
-                                     G_TYPE_INVALID);
-  dbus_g_proxy_add_signal (thumb_proxy, "Ready",
-                           G_TYPE_UINT,
-                           G_TYPE_STRV,
-                           G_TYPE_INVALID);
-
-  dbus_g_object_register_marshaller (mex_marshal_VOID__UINT_POINTER_INT_STRING,
-                                     G_TYPE_NONE,
-                                     G_TYPE_UINT,
-                                     G_TYPE_STRV,
-                                     G_TYPE_INT,
-                                     G_TYPE_STRING,
-                                     G_TYPE_INVALID);
-  dbus_g_proxy_add_signal (thumb_proxy, "Error",
-                           G_TYPE_UINT,
-                           G_TYPE_STRV,
-                           G_TYPE_INT,
-                           G_TYPE_STRING,
-                           G_TYPE_INVALID);
-
-  dbus_g_proxy_connect_signal (thumb_proxy, "Ready", G_CALLBACK (on_ready), NULL, NULL);
-  dbus_g_proxy_connect_signal (thumb_proxy, "Error", G_CALLBACK (on_error), NULL, NULL);
-
-  return TRUE;
-}
-#endif
 
 static char *
 get_mime_type (const char *uri)
@@ -297,24 +196,6 @@ get_mime_type (const char *uri)
   return mime;
 }
 
-#ifdef WITH_THUMBNAILER_TUMBLER
-static void
-on_queue (DBusGProxy *proxy, DBusGProxyCall *call, void *user_data)
-{
-  ClosureData *data = user_data;
-  GError *error = NULL;
-  guint handle = 0;
-
-  if (dbus_g_proxy_end_call (proxy, call, &error, G_TYPE_UINT, &handle, G_TYPE_INVALID)) {
-    g_hash_table_insert (pending, GUINT_TO_POINTER (handle), data);
-  } else {
-    g_message ("Cannot queue thumbnails: %s", error->message);
-    g_error_free (error);
-    g_free (data);
-  }
-}
-#endif
-
 /**
  * mex_thumbnailer_generate: (skip)
  * @url: the URL to thumbnail
@@ -333,47 +214,6 @@ mex_thumbnailer_generate (const char           *url,
                           MexThumbnailCallback  callback,
                           gpointer              user_data)
 {
-#ifdef WITH_THUMBNAILER_INTERNAL
   mex_internal_thumbnail (url, callback, user_data);
   return;
-#endif
-
-#ifdef WITH_THUMBNAILER_TUMBLER
-  char *uris[2], *mimes[2];
-  gboolean free_mime = FALSE;
-  ClosureData *data;
-
-  /* TODO: internal queue? */
-
-  if (!mex_thumbnailer_init ())
-    return;
-
-  /* We can assign @url because DBusGProxy will copy, so just free mimes[0]
-     later */
-  uris[0] = (char *)url;
-  if (mime_type) {
-    mimes[0] = (char *)mime_type;
-  } else {
-    mimes[0] = get_mime_type (url);
-    free_mime = TRUE;
-  }
-  uris[1] = mimes[1] = NULL;
-
-  data = g_new0 (ClosureData, 1);
-  data->callback = callback;
-  data->user_data = user_data;
-
-  dbus_g_proxy_begin_call (thumb_proxy, "Queue",
-                           on_queue, data, NULL,
-                           G_TYPE_STRV, uris,
-                           G_TYPE_STRV, mimes,
-                           G_TYPE_STRING, "x-huge",
-                           G_TYPE_STRING, "default",
-                           G_TYPE_UINT, 0,
-                           G_TYPE_INVALID,
-                           G_TYPE_INVALID);
-
-  if (free_mime)
-    g_free (mimes[0]);
-#endif
 }
