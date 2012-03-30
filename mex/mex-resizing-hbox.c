3d8096c7d9e6a797420fa1a447822eea799d1f38
Thomas Wood <thomas.wood@intel.com>
2012-03-30 15:06:35 +0100

    resizing-hbox: increase the default spacing and set default hdepth to 0.9

--- mex/mex-resizing-hbox.c
+++ mex/mex-resizing-hbox.c
@@ -45,7 +45,7 @@ G_DEFINE_TYPE_WITH_CODE (MexResizingHBox, mex_resizing_hbox, MX_TYPE_WIDGET,
 
 #define INACTIVE_OPACITY 64
 #define ANIMATION_DURATION 500
-#define SPACING 20
+#define SPACING 70
 
 enum
 {
@@ -1712,7 +1712,7 @@ mex_resizing_hbox_class_init (MexResizingHBoxClass *klass)
                               "The multiplier used to determine how much "
                               "children should shrink beyond the child "
                               "designated by the depth-index, horizontally.",
-                              0.f, 1.f, 0.99f,
+                              0.f, 1.f, 0.90f,
                               G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
   g_object_class_install_property (object_class, PROP_HDEPTH_SCALE, pspec);
 
@@ -1818,7 +1818,7 @@ mex_resizing_hbox_init (MexResizingHBox *self)
   priv->alpha = clutter_alpha_new_full (priv->timeline, CLUTTER_EASE_OUT_QUAD);
   priv->resizing_enabled = TRUE;
   priv->depth_index = -1;
-  priv->hdepth = 0.99f;
+  priv->hdepth = 0.90f;
   priv->vdepth = 0.99f;
   priv->max_depth = 5;
   priv->fade = TRUE;
