e9b71fe8e07387b0c999081d0dc95d467f8543ea
Thomas Wood <thomas.wood@intel.com>
2013-03-07 15:12:58 +0000

    Port to gstreamer 1.0

--- thumbnailer/mex-thumbnailer-internal.c
+++ thumbnailer/mex-thumbnailer-internal.c
@@ -61,7 +61,7 @@ main (int argc, char **argv)
                                                        NULL,
                                                        NULL);
 
-          registry = gst_registry_get_default ();
+          registry = gst_registry_get ();
 
           for (i = 0; denied_plugins && denied_plugins[i]; i++)
             {
@@ -141,165 +141,6 @@ mex_internal_thumbnail_image (const gchar *uri,
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses>
  */
-static void
-push_buffer (GstElement *element,
-             GstBuffer  *out_buffer,
-             GstPad     *pad,
-             GstBuffer  *in_buffer)
-{
-  gst_buffer_set_caps (out_buffer, GST_BUFFER_CAPS (in_buffer));
-  GST_BUFFER_SIZE (out_buffer) = GST_BUFFER_SIZE (in_buffer);
-  memcpy (GST_BUFFER_DATA (out_buffer), GST_BUFFER_DATA (in_buffer),
-          GST_BUFFER_SIZE (in_buffer));
-}
-
-static void
-pull_buffer (GstElement *element,
-             GstBuffer  *in_buffer,
-             GstPad     *pad,
-             GstBuffer **out_buffer)
-{
-  *out_buffer = gst_buffer_ref (in_buffer);
-}
-
-static GdkPixbuf *
-convert_buffer_to_pixbuf (GstBuffer    *buffer)
-{
-  GstCaps *pb_caps;
-  GstElement *pipeline;
-  GstBuffer *out_buffer = NULL;
-  GstElement *src, *sink, *colorspace, *scale, *filter;
-  GstBus *bus;
-  GstMessage *msg;
-  gboolean ret;
-  int width, height, dw, dh, i;
-  GstStructure *s;
-
-  s = gst_caps_get_structure (GST_BUFFER_CAPS (buffer), 0);
-  gst_structure_get_int (s, "width", &width);
-  gst_structure_get_int (s, "height", &height);
-
-  if (width > height)
-    {
-      double ratio;
-
-      ratio = (double) THUMBNAIL_SIZE / (double) width;
-      dw = THUMBNAIL_SIZE;
-      dh = height * ratio;
-    }
-  else
-    {
-      double ratio;
-
-      ratio = (double) THUMBNAIL_SIZE / (double) height;
-      dh = THUMBNAIL_SIZE;
-      dw = width * ratio;
-    }
-
-  pb_caps = gst_caps_new_simple ("video/x-raw-rgb",
-                                 "bpp", G_TYPE_INT, 24,
-                                 "depth", G_TYPE_INT, 24,
-                                 "width", G_TYPE_INT, dw,
-                                 "height", G_TYPE_INT, dh,
-                                 "pixel-aspect-ratio", GST_TYPE_FRACTION, 1, 1,
-                                 NULL);
-
-  pipeline = gst_pipeline_new ("pipeline");
-
-  src = gst_element_factory_make ("fakesrc", "src");
-  colorspace = gst_element_factory_make ("ffmpegcolorspace", "colorspace");
-  scale = gst_element_factory_make ("videoscale", "scale");
-  filter = gst_element_factory_make ("capsfilter", "filter");
-  sink = gst_element_factory_make ("fakesink", "sink");
-
-  gst_bin_add_many (GST_BIN (pipeline), src, colorspace, scale,
-                    filter, sink, NULL);
-
-  g_object_set (filter,
-                "caps", pb_caps,
-                NULL);
-  g_object_set (src,
-                "num-buffers", 1,
-                "sizetype", 2,
-                "sizemax", GST_BUFFER_SIZE (buffer),
-                "signal-handoffs", TRUE,
-                NULL);
-  g_signal_connect (src, "handoff",
-                    G_CALLBACK (push_buffer), buffer);
-
-  g_object_set (sink,
-                "signal-handoffs", TRUE,
-                "preroll-queue-len", 1,
-                NULL);
-  g_signal_connect (sink, "handoff",
-                    G_CALLBACK (pull_buffer), &out_buffer);
-
-  ret = gst_element_link (src, colorspace);
-  if (ret == FALSE)
-    {
-      g_warning ("Failed to link src->colorspace");
-      return NULL;
-    }
-
-  ret = gst_element_link (colorspace, scale);
-  if (ret == FALSE)
-    {
-      g_warning ("Failed to link colorspace->scale");
-      return NULL;
-    }
-
-  ret = gst_element_link (scale, filter);
-  if (ret == FALSE)
-    {
-      g_warning ("Failed to link scale->filter");
-      return NULL;
-    }
-
-  ret = gst_element_link (filter, sink);
-  if (ret == FALSE)
-    {
-      g_warning ("Failed to link filter->sink");
-      return NULL;
-    }
-
-  bus = gst_element_get_bus (GST_ELEMENT (pipeline));
-  gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_PLAYING);
-
-  i = 0;
-  msg = NULL;
-  while (msg == NULL && i < 5)
-    {
-      msg = gst_bus_timed_pop_filtered (bus, GST_SECOND,
-                                        GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
-      i++;
-    }
-
-  /* FIXME: Notify about error? */
-  gst_message_unref (msg);
-
-  gst_caps_unref (pb_caps);
-
-  if (out_buffer)
-    {
-      GdkPixbuf *pixbuf;
-      char *data;
-
-      data = g_memdup (GST_BUFFER_DATA (out_buffer),
-                       GST_BUFFER_SIZE (out_buffer));
-      pixbuf = gdk_pixbuf_new_from_data ((guchar *) data,
-                                         GDK_COLORSPACE_RGB, FALSE,
-                                         8, dw, dh, GST_ROUND_UP_4 (dw * 3),
-                                         (GdkPixbufDestroyNotify) g_free,
-                                         NULL);
-
-      gst_buffer_unref (buffer);
-      return pixbuf;
-    }
-
-  /* FIXME: Check what buffers need freed */
-  return NULL;
-}
-
 static GdkPixbuf *
 get_shot (const char *uri)
 {
@@ -308,9 +149,9 @@ get_shot (const char *uri)
   GdkPixbuf *shot = NULL;
   int count = 0;
 
-  playbin = gst_element_factory_make ("playbin2", "playbin");
+  playbin = gst_element_factory_make ("playbin", "playbin");
   audio_sink = gst_element_factory_make ("fakesink", "audiosink");
-  video_sink = gst_element_factory_make ("fakesink", "videosink");
+  video_sink = gst_element_factory_make ("gdkpixbufsink", "videosink");
 
   g_object_set (playbin,
                 "uri", uri,
@@ -339,10 +180,9 @@ get_shot (const char *uri)
       GstFormat format = GST_FORMAT_TIME;
       gint64 duration;
 
-      if (gst_element_query_duration (playbin, &format, &duration))
+      if (gst_element_query_duration (playbin, format, &duration))
         {
           gint64 seekpos;
-          GstBuffer *frame;
 
           if (duration > 0)
             {
@@ -378,16 +218,13 @@ get_shot (const char *uri)
                 }
             }
 
-          g_object_get (playbin,
-                        "frame", &frame,
-                        NULL);
-          if (frame == NULL)
+          g_object_get (video_sink, "last-pixbuf", &shot, NULL);
+
+          if (shot == NULL)
             {
               g_warning ("No frame for %s", uri);
               return NULL;
             }
-
-          shot = convert_buffer_to_pixbuf (frame);
         }
 
     }
@@ -485,15 +322,40 @@ mex_internal_thumbnail_video (const gchar *uri,
 
   if (shot)
     {
+      GdkPixbuf *output;
+      int dw, dh, width, height;
       GError *error = NULL;
 
-      if (gdk_pixbuf_save (shot, thumbnail_path, "jpeg", &error, NULL) == FALSE)
+      width = gdk_pixbuf_get_width (shot);
+      height = gdk_pixbuf_get_height (shot);
+
+      if (width > height)
+        {
+          double ratio;
+
+          ratio = (double) THUMBNAIL_SIZE / (double) width;
+          dw = THUMBNAIL_SIZE;
+          dh = height * ratio;
+        }
+      else
+        {
+          double ratio;
+
+          ratio = (double) THUMBNAIL_SIZE / (double) height;
+          dh = THUMBNAIL_SIZE;
+          dw = width * ratio;
+        }
+
+      output = gdk_pixbuf_scale_simple (shot, dw, dh, GDK_INTERP_HYPER);
+
+      if (gdk_pixbuf_save (output, thumbnail_path, "jpeg", &error, NULL) == FALSE)
         {
           g_warning ("Error writing file %s for %s: %s", thumbnail_path, uri,
                      error->message);
           g_error_free (error);
         }
 
+      g_object_unref (output);
       g_object_unref (shot);
     }
 }
